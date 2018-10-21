f83d741c108cc006bf7579253004300165235779
jjustin25 <37604889+jjustin25@users.noreply.github.com>
2018-10-21 17:50:26 -0400

    Added final step 8 in there

--- SQL_Homework.sql
+++ SQL_Homework.sql
@@ -91,6 +91,30 @@ WHERE language_id in
     FROM language 
     where name='English')
 AND (title LIKE 'K%') OR (title LIKE 'Q%');
+
+-- 8a. In your new role as an executive, you would like to have an easy way of
+
+CREATE VIEW top_five_grossing_genres AS
+
+SELECT name, SUM(p.amount)
+FROM category c
+INNER JOIN film_category fc
+INNER JOIN inventory i
+ON i.film_id = fc.film_id
+INNER JOIN rental r
+ON r.inventory_id = i.inventory_id
+INNER JOIN payment p
+GROUP BY name
+LIMIT 5;
+
+-- 8b. How would you display the view that you created in 8a?
+
+SELECT * FROM top_five_grossing_genres;
+
+-- 8c. You find that you no longer need the view top_five_genres. 
+-- Write a query to delete it.
+
+DROP VIEW top_five_grossing_genres;
  
 
 
